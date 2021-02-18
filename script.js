6471a159630647682a3e7f919a9ddfdf545735cc
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-18 15:05:54 +0000

    add space after // line 7,11,15 add === line 16

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let clicker = 0
 let roulet = 0
 
 function randomNum () {
-  //user inputs a random number and convert into integer
+  // user inputs a random number and convert into integer
   clicker = document.getElementById('table').value
   clicker = parseInt(clicker)
 
-  //random numbers 1-8
+  // random numbers 1-8
   roulet = Math.random() * 8 + 1
-  roulet = parseInt (roulet)
+  roulet = parseInt(roulet)
 
-  //comparing user's guess input with roulet
-  if (roulet == clicker) {
-    document.getElementById('congrats').innerHTML = "Congrats Bro!"
+  // comparing user's guess input with roulet
+  if (roulet === clicker) {
+    document.getElementById('congrats').innerHTML = 'Congrats Bro!'
   }
 }
